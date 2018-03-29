eef3c278af81e8940b07c824364a869075f0decd
Priscila Oliveira <priscilawebdev@gmail.com>
2018-03-30 00:05:45 +0200

    feat: add new modifiers

--- src/components/Cell/index.tsx
+++ src/components/Cell/index.tsx
@@ -4,13 +4,15 @@ import { bm } from 'utils/bem'
 interface ICellProps {
   primary?: boolean
   secondary?: boolean
+  disabled?: boolean
   onClick?: () => void
 }
 
 const Cell = ({
   primary = false,
   secondary = false,
+  disabled = false,
   onClick
-}: ICellProps) => <div className={bm('Cell', { primary, secondary })} onClick={onClick} />
+}: ICellProps) => <div className={bm('Cell', { primary, secondary, disabled })} onClick={onClick} />
 
 export default Cell
