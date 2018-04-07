7877174b3c486616ac1e5f6ebd6e08c7e100f213
Priscila Oliveira <priscilawebdev@gmail.com>
2018-04-07 15:55:26 +0200

    feat: add new matrix 2d, new logic to update cell and reset action

--- src/ducks/game.tsx
+++ src/ducks/game.tsx
@@ -1,13 +1,13 @@
 import { Reducer } from 'redux'
 
 export interface IGame {
-  matrix: number[],
-  userCurrentPlayer: boolean,
+  matrix: number[][],
+  userCurrentPlayer: boolean, // Computer has 1 value and Human has 2 value
   score: number[]
 }
 
-const InitialState = {
-  matrix: Array<number>(42).fill(0),
+const InitialState: IGame = {
+  matrix: Array<number>(7).fill(0).map(() => Array<number>(7).fill(0)),
   userCurrentPlayer: false,
   score: [0, 0]
 }
@@ -17,21 +17,27 @@ export const actions = {
   SET_WINNER: 'SET_WINNER',
   SET_DRAW: 'SET_DRAW',
   RESET: 'RESET',
-  setScore: (position: number) => ({
+  setScore: (col: number, row: number) => ({
     type: actions.SET_SCORE,
-    position
+    position: { col, row }
+  }),
+  reset: () => ({
+    type: actions.RESET
   })
 }
 
-const reducer: Reducer<IGame> = (state: IGame = InitialState, { type, position }) => {
+const reducer: Reducer<IGame> = (state = InitialState, { type, position }) => {
   switch (type) {
     case actions.SET_SCORE:
-      const currentPLayer = state.userCurrentPlayer ? 1 : 2
+      const matrix = JSON.parse(JSON.stringify(state.matrix))
+      matrix[position.col][position.row] = state.userCurrentPlayer ? 2 : 1
       return {
         ...state,
-        matrix: state.matrix.map((item, index) => index === position ? currentPLayer : item),
+        matrix,
         userCurrentPlayer: !state.userCurrentPlayer
       }
+    case actions.RESET:
+      return InitialState
     default:
       return state
   }
