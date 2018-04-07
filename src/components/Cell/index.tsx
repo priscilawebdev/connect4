2504bd82d79f5af5ff8cc1ec5efaf2751d796449
Priscila Oliveira <priscilawebdev@gmail.com>
2018-04-07 16:00:21 +0200

    feat: add new modifiers and js/css effect for the matrix's header

--- src/components/Cell/index.tsx
+++ src/components/Cell/index.tsx
@@ -1,18 +1,78 @@
-import React from 'react'
-import { bm } from 'utils/bem'
+import React, { Component } from 'react'
+import { bm, be } from 'utils/bem'
 
 interface ICellProps {
   primary?: boolean
   secondary?: boolean
   disabled?: boolean
-  onClick?: () => void
+  heading?: boolean
+  col: number
+  onClick: (col: number, row: number) => { type: string, position: { col: number, row: number } }
+  matrix: number[][]
 }
 
-const Cell = ({
-  primary = false,
-  secondary = false,
-  disabled = false,
-  onClick
-}: ICellProps) => <div className={bm('Cell', { primary, secondary, disabled })} onClick={onClick} />
+interface ICellState {
+  top: number
+  drop: boolean
+}
+
+const initialState: ICellState = {
+  top: 4,
+  drop: false
+}
+
+class Cell extends Component<ICellProps, ICellState> {
+  state = initialState
+
+  getRow = () => {
+    const { matrix, col } = this.props
+    let row = matrix.length - 1
+    while (matrix[col][row]) {
+      row--
+    }
+    return row
+  }
+
+  reset = () => (
+    new Promise((resolve) => {
+      setTimeout(() => {
+        this.setState(initialState, () => resolve())
+      }, 400)
+    })
+  )
+
+  handlerClick = () => {
+    const { col, onClick } = this.props
+    const cellIndex = this.getRow()
+    this.setState({
+      top: (cellIndex * 60) + 8,
+      drop: true
+    }, () => {
+      this.reset().then(() => onClick(col, cellIndex))
+    })
+  }
+
+  getMoves = () => {
+    const { top } = this.state
+    const moves = ['top4', 'top68', 'top128', 'top188', 'top248', 'top308', 'top368']
+    const filteredMoves = moves.findIndex(move => move === `top${top}`)
+    return { [moves[filteredMoves]]: true }
+  }
+
+  render() {
+    const { primary = false, secondary = false, disabled = false, heading = false } = this.props
+    const { drop } = this.state
+    return (
+      <div
+        className={bm('Cell', { primary, secondary, disabled: drop || disabled, heading })}
+        onClick={this.handlerClick}
+      >
+        {heading && (
+          <div className={be('Cell', 'inner', { drop, ...this.getMoves() })} />
+        )}
+      </div>
+    )
+  }
+}
 
 export default Cell
