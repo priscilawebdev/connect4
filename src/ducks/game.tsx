e1b47465f4a923ca17082ced21adb78b5b4f1d08
Priscila Oliveira <priscilawebdev@gmail.com>
2018-04-08 23:37:49 +0200

    feat: add status state and status setter

--- src/ducks/game.tsx
+++ src/ducks/game.tsx
@@ -1,40 +1,51 @@
 import { Reducer } from 'redux'
 
+export type TStatus = 'initial' | 'running' | 'winner' | 'draw'
+
 export interface IGame {
   matrix: number[][],
-  userCurrentPlayer: boolean, // Computer has 1 value and Human has 2 value
-  score: number[]
+  player: number, // Computer has 1 value and Human has 2 value
+  status: TStatus
 }
 
 const InitialState: IGame = {
   matrix: Array<number>(7).fill(0).map(() => Array<number>(7).fill(0)),
-  userCurrentPlayer: false,
-  score: [0, 0]
+  player: 1,
+  status: 'initial'
 }
 
 export const actions = {
   SET_SCORE: 'SET_SCORE',
-  SET_WINNER: 'SET_WINNER',
-  SET_DRAW: 'SET_DRAW',
+  SET_STATUS: 'SET_STATUS',
   RESET: 'RESET',
   setScore: (col: number, row: number) => ({
     type: actions.SET_SCORE,
     position: { col, row }
+  }),
+  setStatus: (status: TStatus) => ({
+    type: actions.SET_STATUS,
+    status
   }),
   reset: () => ({
     type: actions.RESET
   })
 }
 
-const reducer: Reducer<IGame> = (state = InitialState, { type, position }) => {
+const reducer: Reducer<IGame> = (state = InitialState, { type, position, status }) => {
   switch (type) {
     case actions.SET_SCORE:
       const matrix = JSON.parse(JSON.stringify(state.matrix))
-      matrix[position.col][position.row] = state.userCurrentPlayer ? 2 : 1
+      matrix[position.col][position.row] = state.player
       return {
         ...state,
         matrix,
-        userCurrentPlayer: !state.userCurrentPlayer
+        player: state.player === 1 ? 2 : 1,
+        status: 'running'
+      }
+    case actions.SET_STATUS:
+      return {
+        ...state,
+        status
       }
     case actions.RESET:
       return InitialState
