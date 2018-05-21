6480961ad80be287f9b1c963a70069e7a3391fff
Priscila Oliveira <priscilawebdev@gmail.com>
2018-05-21 22:36:25 +0200

    add: added Spinner component

---
+++ src/components/Spinner/index.tsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import { bm } from 'utils/bem'
+
+const Spinner = () => <div className={bm('Spinner')} />
+
+export default Spinner
--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { PersistGate } from 'redux-persist/integration/react'
 import { Provider } from 'react-redux'
-import 'utils/styles/index.sass'
+import Spinner from 'components/Spinner'
 import Layout from 'components/Layout'
 import Home from 'containers/Home'
+import 'utils/styles/index.sass'
 import { store, persistor } from './store'
 
 const Main = () => (
   <Provider store={store}>
-    <PersistGate loading={'loading...'} persistor={persistor}>
+    <PersistGate loading={<Spinner />} persistor={persistor}>
       <Layout>
         <Home />
       </Layout>
